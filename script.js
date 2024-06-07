a57f4ea0981c8ffd269413255000016851f45602
Colin <casualcriesforhelp@gmail.com>
2024-06-06 22:08:12 -0300

    gallery update

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-const myCarouselElement = document.querySelector('#carouselOne')
+const myCarouselElement3 = document.querySelector('#carouselThree');
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
+const carousel3 = new bootstrap.Carousel(myCarouselElement3, {
   interval: 2000,
   touch: false
-})
+});
+
+
+const carousel2 = new bootstrap.Carousel(myCarouselElement2, {
+    interval: 2000,
+    touch: false
+  });
+const myCarouselElement2 = document.querySelector('#carouselTwo');
+
+const myCarouselElement1 = document.querySelector('#carousel1');
+
+const carousel1 = new bootstrap.Carousel(myCarouselElement1, {
+  interval: 2000,
+  touch: false
+});
+
+
+
+
+
+
